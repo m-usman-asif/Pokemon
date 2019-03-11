9764fffe7c8683da460c0b79244ddedf535b6ae2
Usman Asif <musmanasif218@gmail.com>
2019-03-11 14:48:22 +0500

    Initial commit

---
+++ src/app/data.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable} from 'rxjs';
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  url: any = 'https://pokeapi.co/api/v2/pokemon/';
+
+  //abilityUrl: any = 'https://pokeapi.co/api/v2/pokemon/1';
+
+
+
+  constructor(private http: HttpClient) { }
+
+  getPokemons(): Observable<any> {
+    return this.http.get(this.url);
+  }
+
+  getAbilities(url): Observable<any> {
+    return this.http.get(url);
+  }
+
+}
+
+
+
+
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,8 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+import { HttpClient } from '@angular/common/http';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {}
+export class Tab1Page implements OnInit {
+
+  pokeList: any = { };
+
+  detailsPoke = null;
+
+  constructor(private data: DataService,
+              private http: HttpClient,
+              private activatedRoute: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.data.getPokemons().subscribe(result => {
+      this.pokeList = result;
+      console.log(this.pokeList);
+    });
+  }
+
+  newUrl(url) {
+    // console.log(this.http.get(url.url));
+    this.data.getAbilities(url.url).subscribe(result => {
+      this.pokeList = result;
+      console.log(this.pokeList);
+    });
+  }
+  
+
+}
